a3d62205defb226b6664c5eeddd31da22cab8fea
Andrews Medina <andrewsmedina@gmail.com>
2014-02-25 18:46:19 -0300

    implemented add_url.

--- healthcheck/backends/__init__.py
+++ healthcheck/backends/__init__.py
@@ -1,3 +1,41 @@
+import os
+
+
+def get_value(key):
+    try:
+        value = os.environ[key]
+    except KeyError:
+        msg = u"You must define the {} " \
+              u"environment variable.".format(key)
+        raise Exception(msg)
+    return value
+
+
 class Zabbix(object):
+    def __init__(self):
+        url = get_value("ZABBIX_URL")
+        user = get_value("ZABBIX_USER")
+        password = get_value("ZABBIX_PASSWORD")
+        self.host_id = get_value("ZABBIX_HOST")
+        from pyzabbix import ZabbixAPI
+        self.zapi = ZabbixAPI(url)
+        self.zapi.login(user, password)
+
     def add_url(self, url):
-        pass
+        name = "healthcheck for {}".format(url)
+        self.zapi.httptest.create(
+            name=name,
+            steps=[{
+                "name": name,
+                "url": url,
+                "status_codes": 200,
+                "no": 1,
+            }],
+            hostid=self.host_id,
+        )
+        expression = "{{Zabbix Server:web.test.rspcode[{},{}].last()}}#200"
+        self.zapi.trigger.create(
+            description="trigger for url ".format(url),
+            expression=expression.format(name, name),
+            priority=5,
+        )
--- tests/backends/test_zabbix.py
+++ tests/backends/test_zabbix.py
@@ -1,10 +1,30 @@
 from unittest import TestCase
 
-from healthcheck.backends import Zabbix
+import mock
+import os
 
 
 class ZabbixTest(TestCase):
+    def remove_env(self, env):
+        if env in os.environ:
+            del os.environ[env]
+
+    @mock.patch("pyzabbix.ZabbixAPI")
+    def setUp(self, zabbix_mock):
+        url = "http://zbx.com"
+        user = "user"
+        password = "pass"
+        os.environ["ZABBIX_URL"] = url
+        os.environ["ZABBIX_USER"] = user
+        os.environ["ZABBIX_PASSWORD"] = password
+        os.environ["ZABBIX_HOST"] = "1"
+        zapi_mock = mock.Mock()
+        zabbix_mock.return_value = zapi_mock
+        from healthcheck.backends import Zabbix
+        self.backend = Zabbix()
+        zabbix_mock.assert_called_with(url)
+        zapi_mock.login.assert_called_with(user, password)
+
     def test_add_url(self):
-        backend = Zabbix()
         url = "http://mysite.com"
-        backend.add_url(url)
+        self.backend.add_url(url)
