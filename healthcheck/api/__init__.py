8fa3046c5b25b1003ee0a00e85992de58d245849
Andrews Medina <andrewsmedina@gmail.com>
2014-04-14 18:47:41 -0300

    remove unused code.

--- healthcheck/api/__init__.py
+++ healthcheck/api/__init__.py
@@ -6,31 +6,6 @@ import inspect
 app = Flask(__name__)
 
 
-@app.route("/resources/<name>", methods=["POST"])
-def bind(name):
-    return "", 201
-
-
-@app.route("/resources/<name>/hostname/<host>", methods=["DELETE"])
-def unbind(name, host):
-    return "", 200
-
-
-@app.route("/resources", methods=["POST"])
-def add_instance():
-    return "", 201
-
-
-@app.route("/resources/<name>", methods=["DELETE"])
-def remove_instance(name):
-    return "", 200
-
-
-@app.route("/resources/<name>/status", methods=["GET"])
-def status(name):
-    return "", 204
-
-
 @app.route("/url", methods=["POST"])
 def add_url():
     from healthcheck.backends import Zabbix
