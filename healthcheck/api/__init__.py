d2f806d3cf15edb7f61fffd0be48208cf44760f0
Andrews Medina <andrewsmedina@gmail.com>
2014-04-14 21:32:59 -0300

    refactored api.

--- healthcheck/api/__init__.py
+++ healthcheck/api/__init__.py
@@ -6,56 +6,49 @@ import inspect
 app = Flask(__name__)
 
 
+def zabbix():
+    from healthcheck.backends import Zabbix
+    return Zabbix()
+
+
 @app.route("/url", methods=["POST"])
 def add_url():
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
     name = request.form.get("name")
     url = request.form.get("url")
-    zabbix.add_url(name, url)
+    zabbix().add_url(name, url)
     return "", 201
 
 
 @app.route("/<name>/url/<path:url>", methods=["DELETE"])
 def remove_url(name, url):
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
-    zabbix.remove_url(name, url)
+    zabbix().remove_url(name, url)
     return "", 204
 
 
 @app.route("/watcher", methods=["POST"])
 def add_watcher():
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
     name = request.form.get("name")
     watcher = request.form.get("watcher")
-    zabbix.add_watcher(name, watcher)
+    zabbix().add_watcher(name, watcher)
     return "", 201
 
 
 @app.route("/<name>/watcher/<watcher>", methods=["DELETE"])
 def remove_watcher(name, watcher):
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
-    zabbix.remove_watcher(name, watcher)
+    zabbix().remove_watcher(name, watcher)
     return "", 204
 
 
 @app.route("/", methods=["POST"])
 def new():
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
     name = request.form.get("name")
-    zabbix.new(name)
+    zabbix().new(name)
     return "", 201
 
 
 @app.route("/<name>", methods=["DELETE"])
 def remove(name):
-    from healthcheck.backends import Zabbix
-    zabbix = Zabbix()
-    zabbix.remove(name)
+    zabbix().remove(name)
     return "", 204
 
 
