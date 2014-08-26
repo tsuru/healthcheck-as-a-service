b39c281e64efa1a87ae4bbff9636b1f727129859
Andrews Medina <andrewsmedina@gmail.com>
2014-08-26 18:15:09 -0300

    plugin: fixed request.

--- healthcheck/plugin.py
+++ healthcheck/plugin.py
@@ -28,7 +28,7 @@ class Request(urllib2.Request):
 
     def __init__(self, method, *args, **kwargs):
         self._method = method
-        urllib2.Request.__init__(*args, **kwargs)
+        urllib2.Request.__init__(self, *args, **kwargs)
 
     def get_method(self):
         return self._method
