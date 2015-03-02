bf784894e227368d223b5a4e253ebc2b4a9866dd
Francisco Souza <fss@corp.globo.com>
2015-03-02 17:45:42 -0300

    all: update year in copyright banner

--- tests/managers.py
+++ tests/managers.py
@@ -1,4 +1,4 @@
-# Copyright 2014 healthcheck-as-a-service authors. All rights reserved.
+# Copyright 2015 healthcheck-as-a-service authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
