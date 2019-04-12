5dce92c710b9baa8acf6f9592260dd1cf5a90081
Rafael Bernardo <rafaelbernardo@protonmail.com>
2019-04-12 12:33:07 -0300

    SSL: CERTIFICATE_VERIFY_FAILED with Python2
    because new ssl endpoint of tsuru we need this to avoid errors with certificate

--- healthcheck/plugin.py
+++ healthcheck/plugin.py
@@ -8,7 +8,9 @@ import json
 import os
 import sys
 import urllib2
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def get_env(name):
     env = os.environ.get(name)
